d794ab15849d9201df9254f456c2dce940ea84bc
PGJ <pol.gozalvez@mataro.epiaedu.cat>
2024-11-03 20:00:54 +0100

    Final implementation

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const $inputTitle = window.document.querySelector("#title")
-const $inputContent = window.document.querySelector("#content")
-const $selectPriority = window.document.querySelector("#priority")
+const $inputTitle = document.querySelector("#title");
+const $inputContent = document.querySelector("#content");
+const $selectPriority = document.querySelector("#priority");
+const $taskList = document.querySelector("#tasklist");
 
-const $button = window.document.querySelector("button")
+const $button = document.querySelector("button");
 $button.addEventListener("click", function(event) {
     event.preventDefault();
-    
-    window.console.log($inputTitle.value);
-    window.console.log($inputContent.value);
-    window.console.log($selectPriority.value);
+
+    const $listItem = document.createElement("div");
+    $listItem.classList.add("div__container");
+    //console.log($listItem);
+
+    const $titleElement = document.createElement("h3");
+    $titleElement.textContent = `${$inputTitle.value}`;
+    $listItem.appendChild($titleElement);
+
+    const $contentElement = document.createElement("p");
+    $contentElement.textContent = `${$inputContent.value}`;
+    $listItem.appendChild($contentElement);
+
+    const $priorityElement = document.createElement("p");
+    $priorityElement.textContent = `${$selectPriority.value}`;
+    $listItem.appendChild($priorityElement);
+
+    $taskList.appendChild($listItem);
+
+    $inputTitle.value = "";
+    $inputContent.value = "";
+    $selectPriority.value = "Priority";
 });
